b00e5f8de2cf0c515d6b5a5db141f9715cd9424f
Hugo Martinez <hugo@yeezers.com>
2018-10-16 09:50:25 +0200

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,10 +1,11 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-const todos = [{    text: 'First test todo' }, { text: 'Second test todo' } ];
+const todos = [{_id: new ObjectID(),   text: 'First test todo' }, {_id: new ObjectID(), text: 'Second test todo' } ];
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
@@ -62,6 +63,21 @@ describe ('GET /todos', () => {
         .expect(200)
         .expect((res) => {
             expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });    
+
+});
+
+console.log(`/todos/${todos[0]._id.toHexString()}`);
+
+describe ('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text);
         })
         .end(done);
     });    
