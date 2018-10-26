ce198d8577bc9780f4ab4eb5893ccd8682ff0b11
Hugo Martinez <hugo@yeezers.com>
2018-10-26 23:09:19 +0200

    Setup for Heroku 4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect(proces.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-mongoose.connect('mongodb://heroku_qg25gg7g:Lays1712@ds153657.mlab.com:53657/heroku_qg25gg7g');
+mongoose.connect('mongodb://hma:1712hugoma@ds153657.mlab.com:53657/heroku_qg25gg7g');
 
 module.exports = {
     mongoose: mongoose
