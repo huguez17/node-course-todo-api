2f0f330f48395c339100aa03426f0f683cbcfa59
Hugo Martinez <hugo@yeezers.com>
2018-11-11 12:37:05 +0100

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,16 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+
+//Not Working in Heroku :(
 //mongoose.connect(proces.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://hma:1712hugoma@ds153657.mlab.com:53657/heroku_qg25gg7g');
+//Temp commented
+//mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://hma:1712hugoma@ds153657.mlab.com:53657/heroku_qg25gg7g');
+
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
     mongoose: mongoose
 };
+
