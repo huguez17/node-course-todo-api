2b6573ab84364dea8554ae8b88c126a0ca002e55
Hugo Martinez <hugo@yeezers.com>
2018-11-25 16:46:41 +0100

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -230,7 +230,7 @@ describe('POST /users' , () => {
 
 });
 
-describe('POST /users/login', () =>{
+describe('POST /users/login', () => {
     it('should login user and return auth token', (done) => {
         request(app)
             .post('/users/login')
@@ -280,4 +280,26 @@ describe('POST /users/login', () =>{
             });
     });
 
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        var theToken = "";
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            })
+
+    });
 });
